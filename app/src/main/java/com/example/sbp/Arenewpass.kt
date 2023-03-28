c5f9bd1c110241a2264ae6c8abea0b26f665ee0a
meetp <meetmpatel20@gnu.ac.in>
2023-03-28 10:14:18 +0530

    intial comment

---
+++ app/src/main/java/com/example/sbp/Arenewpass.kt
@@ -0,0 +1,11 @@
+package com.example.sbp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Arenewpass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arenewpass)
+    }
+}
